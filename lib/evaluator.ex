ef90714d9aa695bb29f17372f3fa44ad6b07f0d1
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-06-11 17:45:35 -0400

    WIP

--- lib/evaluator.ex
+++ lib/evaluator.ex
@@ -11,8 +11,22 @@ defmodule Evaluator do
   def eval({:number, number}), do: number
   def eval({:add, a_expr, b_expr}), do: eval(a_expr) + eval(b_expr)
   def eval({:mul, a_expr, b_expr}), do: eval(a_expr) * eval(b_expr)
+  def eval({:div, _a_expr, 0}), do: raise("Invalid div operation")
   def eval({:div, a_expr, b_expr}), do: eval(a_expr) / eval(b_expr)
-  def eval({:sub, a_expr, b_expr}), do: eval(a_expr) - eval(b_expr)
-  def eval({:pow, a_expr, b_expr}), do: :math.pow(eval(a_expr), eval(b_expr))
+  def eval({:sub, a_expr, b_expr}), do: sub(eval(a_expr), eval(b_expr))
+  def eval({:pow, a_expr, b_expr}), do: pow(eval(a_expr), eval(b_expr))
   def eval(_invalid_expr), do: raise("Invalid expression")
+
+  def sub(a_number, b_number) when b_number < 0, do: a_number + b_number
+  def sub(a_number, b_number), do: a_number - b_number
+
+  def pow(a_number, b_number)
+      when is_integer(a_number) and is_integer(b_number) and a_number >= 0 and b_number >= 0,
+      do: Integer.pow(a_number, b_number)
+
+  def pow(a_number, b_number) do
+    a_number_float = a_number / 1
+    b_number_float = b_number / 1
+    Float.pow(a_number_float, b_number_float)
+  end
 end
--- test/evaluator_test.exs
+++ test/evaluator_test.exs
@@ -9,14 +9,102 @@ defmodule EvaluatorTest do
 
   describe "eval/1 with add expression" do
     test "with basic number expression" do
-      assert 2 == Evaluator.eval({:add, {:number, 1}, {:number, 1}})
+      [number_1, number_2] = list_of_random_numbers(2)
+
+      assert number_1 + number_2 ==
+               Evaluator.eval({:add, {:number, number_1}, {:number, number_2}})
     end
 
     test "with another expressions" do
-      assert 10 ==
+      [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+
+      assert number_1 + number_2 + (number_3 - number_4) ==
                Evaluator.eval(
-                 {:add, {:add, {:number, 1}, {:number, 3}}, {:add, {:number, 3}, {:number, 3}}}
+                 {:add, {:add, {:number, number_1}, {:number, number_2}},
+                  {:sub, {:number, number_3}, {:number, number_4}}}
                )
     end
   end
+
+  describe "eval/1 with mul expression" do
+    test "with basic number expression" do
+      [number_1, number_2] = list_of_random_numbers(2)
+
+      assert number_1 * number_2 ==
+               Evaluator.eval({:mul, {:number, number_1}, {:number, number_2}})
+    end
+
+    test "with another expressions" do
+      [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+
+      assert number_1 * number_2 * (number_3 + number_4) ==
+               Evaluator.eval(
+                 {:mul, {:mul, {:number, number_1}, {:number, number_2}},
+                  {:add, {:number, number_3}, {:number, number_4}}}
+               )
+    end
+  end
+
+  describe "eval/1 with div expression" do
+    test "with basic number expression" do
+      [number_1, number_2] = list_of_random_numbers(2)
+
+      assert number_1 / number_2 ==
+               Evaluator.eval({:div, {:number, number_1}, {:number, number_2}})
+    end
+
+    test "with another expressions" do
+      [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+
+      # assert number_1 * number_2 / (number_3 - number_4) ==
+      Evaluator.eval(
+        {:div, {:mul, {:number, number_1}, {:number, number_2}},
+         {:sub, {:number, number_3}, {:number, number_4}}}
+      )
+
+      assert true
+    end
+  end
+
+  describe "eval/1 with sub expression" do
+    test "with basic number expression" do
+      [number_1, number_2] = list_of_random_numbers(2)
+
+      assert number_1 - number_2 ==
+               Evaluator.eval({:sub, {:number, number_1}, {:number, number_2}})
+    end
+
+    test "with another expressions" do
+      [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+
+      assert number_1 * number_2 - (number_3 + number_4) ==
+               Evaluator.eval(
+                 {:sub, {:mul, {:number, number_1}, {:number, number_2}},
+                  {:add, {:number, number_3}, {:number, number_4}}}
+               )
+    end
+  end
+
+  describe "eval/1 with pow expression" do
+    test "with basic number expression" do
+      [number_1, number_2] = list_of_random_numbers(2)
+
+      assert Evaluator.pow(number_1, number_2) ==
+               Evaluator.eval({:pow, {:number, number_1}, {:number, number_2}})
+    end
+
+    test "with another expressions" do
+      [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+
+      assert Evaluator.pow(number_1 * number_2, number_3 + number_4) ==
+               Evaluator.eval(
+                 {:pow, {:mul, {:number, number_1}, {:number, number_2}},
+                  {:add, {:number, number_3}, {:number, number_4}}}
+               )
+    end
+  end
+
+  # defp random_number, do: Enum.random(-100..0)
+  defp random_number, do: -1000
+  defp list_of_random_numbers(number), do: for(_ <- 1..number, do: random_number())
 end
