91b34f9473f3cc030266a36649428330d265fdaa
Alessandro Teiji <teijiwatanabe01@gmail.com>
2022-06-12 09:10:13 -0400

    Fix broken tests

--- test/evaluator_test.exs
+++ test/evaluator_test.exs
@@ -17,8 +17,9 @@ defmodule EvaluatorTest do
 
     test "with another expressions" do
       [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+      function = if number_4 < 0, do: &Kernel.+/2, else: &Kernel.-/2
 
-      assert number_1 + number_2 + (number_3 - number_4) ==
+      assert number_1 + number_2 + function.(number_3, number_4) ==
                Evaluator.eval(
                  {:add, {:add, {:number, number_1}, {:number, number_2}},
                   {:sub, {:number, number_3}, {:number, number_4}}}
@@ -69,15 +70,17 @@ defmodule EvaluatorTest do
   describe "eval/1 with sub expression" do
     test "with basic number expression" do
       [number_1, number_2] = list_of_random_numbers(2)
+      function = if number_2 < 0, do: &Kernel.+/2, else: &Kernel.-/2
 
-      assert number_1 - number_2 ==
+      assert function.(number_1, number_2) ==
                Evaluator.eval({:sub, {:number, number_1}, {:number, number_2}})
     end
 
     test "with another expressions" do
       [number_1, number_2, number_3, number_4] = list_of_random_numbers(4)
+      function = if number_3 + number_4 < 0, do: &Kernel.+/2, else: &Kernel.-/2
 
-      assert number_1 * number_2 - (number_3 + number_4) ==
+      assert function.(number_1 * number_2, number_3 + number_4) ==
                Evaluator.eval(
                  {:sub, {:mul, {:number, number_1}, {:number, number_2}},
                   {:add, {:number, number_3}, {:number, number_4}}}
@@ -104,7 +107,6 @@ defmodule EvaluatorTest do
     end
   end
 
-  # defp random_number, do: Enum.random(-100..0)
-  defp random_number, do: -1000
+  defp random_number, do: Enum.random(-1000..1000)
   defp list_of_random_numbers(number), do: for(_ <- 1..number, do: random_number())
 end
